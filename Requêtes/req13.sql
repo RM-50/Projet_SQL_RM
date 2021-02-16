f9f91914c88a7d8b6003a0f05c2e0c1cbb3d4795
RM-50 <78910633+RM-50@users.noreply.github.com>
2021-02-16 16:29:05 +0100

    Add files via upload

---
+++ Requêtes/req13.sql
@@ -0,0 +1,8 @@
+SELECT primaryTitle
+FROM title_basics
+JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
+WHERE genres like '%Animation%' 
+AND titleType = 'movie'
+AND title_ratings.numVotes > 1000
+ORDER BY averageRating DESC
+LIMIT 10
