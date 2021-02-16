f998e7c2f44c553406f7de0c9b8dbbd6ae783101
RM-50 <78910633+RM-50@users.noreply.github.com>
2021-02-16 14:16:20 +0100

    Add files via upload

---
+++ Requêtes/req9.sql
@@ -0,0 +1,10 @@
+SELECT primaryName
+FROM name_basics
+WHERE nconst IN(SELECT writers
+				FROM title_writers
+				WHERE tconst IN(SELECT tconst
+								FROM title_basics
+								WHERE primaryTitle='Taxi' and startYear='1998'))
+				
+
+											
