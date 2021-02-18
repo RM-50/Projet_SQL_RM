b5882d085ff9208408214edc7d88c74627f623b4
RM-50 <78910633+RM-50@users.noreply.github.com>
2021-02-18 13:06:35 +0100

    Add files via upload

---
+++ Requêtes/req18.sql
@@ -0,0 +1,7 @@
+SELECT primaryTitle
+FROM title_basics
+WHERE tconst IN(
+SELECT knownForTitles
+FROM name_titles
+JOIN name_basics ON name_titles.nconst = name_basics.nconst
+WHERE name_basics.primaryName = 'Sean Connery')
