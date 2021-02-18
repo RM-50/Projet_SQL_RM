c533f4a043d32e91d411e780359c2a5ef80eca65
RM-50 <78910633+RM-50@users.noreply.github.com>
2021-02-18 14:44:43 +0100

    Add files via upload

--- Lecture_requêtes.py
+++ Lecture_requêtes.py
@@ -1,7 +1,19 @@
 import sqlite3
-conn = sqlite3.connect('imdb.db')
-c = conn.cursor()
-c.execute("select * from title_basics limit 10")
-for row in c:
-    print(row)
-conn.close()
+import os
+
+
+
+def execute(req):
+    conn = sqlite3.connect('imdb.db')
+    c = conn.cursor()
+    c.execute(req)
+    for row in c:
+        print(row)
+    conn.close()
+    
+#print(execute("select * from title_basics limit 10"))
+
+def existe(repertoire):
+    return os.path.isdir(repertoire)
+
+print(existe('requete'))
